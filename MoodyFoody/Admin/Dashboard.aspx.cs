1a4e489829e44de5036618ad007463e5422cf976
ADITYA <ADITYA@ADITYA-PC>
2024-03-07 14:54:26 +0530

    Add project files.

---
+++ MoodyFoody/Admin/Dashboard.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MoodyFoody.Admin
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["breadcrumb"] = "";
+                if (Session["admin"] == null)
+                {
+                    Response.Redirect("../User/Login.aspx");
+                }
+                else
+                {
+                    DashboardCount dashboard = new DashboardCount();
+                    Session["category"] = dashboard.Count("CATEGORY");
+                    Session["product"] = dashboard.Count("PRODUCT");
+                    Session["order"] = dashboard.Count("ORDER");
+                    Session["delivered"] = dashboard.Count("DELIVERED");
+                    Session["pending"] = dashboard.Count("PENDING");
+                    Session["user"] = dashboard.Count("USER");
+                    Session["soldAmount"] = dashboard.Count("SOLDAMOUNT");
+                    Session["contact"] = dashboard.Count("CONTACT");
+                }
+
+            }
+        }
+    }
+}
